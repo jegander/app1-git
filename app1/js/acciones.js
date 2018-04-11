4ae22ed88693a5f62197081b7c7cd8a4781146c3
ligartijax3 <38224756+ligartijax3@users.noreply.github.com>
2018-04-11 13:21:19 -0500

    2subida

--- app1/js/acciones.js
+++ app1/js/acciones.js
@@ -6,7 +6,7 @@
 		$('#disp table td').eq(5).text(device.cordova);
 		$('#disp table td').eq(7).text(device.platform);
 		$('#disp table td').eq(9).text(device.version);
-		$('#disp table td').eq(3).text(device.uuid);
+		$('#disp table td').eq(11).text(device.uuid);
 	document.addEventListener("pause",function(){
 		escribehistoria('la app se pauso');
 	},false);//pause
